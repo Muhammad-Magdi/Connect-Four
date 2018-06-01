a4e08ecb096a3217c23043eb6cc1cf1bda0a7c5a
Manar Arabi <manar.araby.ma@gmail.com>
2018-06-01 14:33:53 +0200

    Update UserInterface.h

--- UserInterface.h
+++ UserInterface.h
@@ -1,31 +1,46 @@
 #include "ConnectFour.h"
+#include "TExaS.h"
 #include "Nokia5110.h"
 
-void welcomeScene (void);
+
+//************************display functions**********************
+
+//nokia init
+void StuffInit (void);
+
+
+void WelcomeScene (void);
 
 //Selects either single or multi. mode
 // 1 = single, 2 = multi
-int selectModeScene (void);
+int SelectModeScene (void);
+
+//Selects either Master or Slave
+// 1 = Master, 2 = Salve
+int MSScene (void);
 
 //select seed
 //Selects either player1 = X or player2 = O
-char selectSeedScene();
+char SelectSeedScene(void);
+
 
 //returns 1 if the user choosed to be the master, 0 otherwise
-
-int isMaster();
+//int isMaster();
 
 //Draws an empty ROWS*COLS grid
-void drawEmptyGrid();
+void drawEmptyGrid(void);
 
 //Draws the seed[X, O] int column number col
 void drawSeed(char seed, int col);
 
 //Waiting Screen
-void waitingScene();
+void WaitingScene(void);
 
 //Prints a screen with the given game state
-void stateScene(GameState gameState);
+//void StateScene(GameState gameState);
 
 //"You are player 1"
 void displaySlaveSeed (int turn) ;
+
+// time delay in 0.1 seconds
+void Delay100ms(unsigned long count); 
